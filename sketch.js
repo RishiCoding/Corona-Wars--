c5b1f3bb4906ee209ef34961495215d66a6c808f
RishiCoding <76730585+RishiCoding@users.noreply.github.com>
2021-09-11 14:31:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-var path, boy, Coin1, liquid;
+var path, sanitizer, Coin1, liquid;
 var pathImg,boyImg, carImg, coinGif, liquidImg;
 var Car;
 var PLAY=1;
 var END=0;
 var gameState=1;
 var score=0;
+var newScore=0;
 
 function preload(){
   pathImg = loadImage("sky background.png");
@@ -24,15 +25,15 @@ function setup() {
   path.scale=4;
   path.velocityY = 0;
   
-  boy = createSprite(width-70,height-100,20,20);
-  boy.addImage("SahilRunning",boyImg);
-  boy.scale=0.12;
+  sanitizer = createSprite(width-70,height-100,20,20);
+  sanitizer.addImage("SahilRunning",boyImg);
+  sanitizer.scale=0.12;
 
   
   
   
-  CarGroup=new Group();
-  CoinGroup=new Group();
+  virusGroup=new Group();
+  vaccineGroup=new Group();
   
 }
 
@@ -43,20 +44,24 @@ function draw() {
   
   if(gameState===PLAY){
   background(0);
-  boy.x = World.mouseX;
+  sanitizer.x = World.mouseX;
   
   edges= createEdgeSprites();
-  boy.collide(edges);
+  sanitizer.collide(edges);
   
   //code to reset the background
   
   
-  if(CoinGroup.isTouching(boy)){
-    CoinGroup.destroyEach();
+  if(vaccineGroup.isTouching(sanitizer)){
+    vaccineGroup.destroyEach();
     score=score+1;
   }  
+
+  if(newScore>score){
+    newScore=score;
+  }
     
-  if(CarGroup.isTouching(boy)){
+  if(virusGroup.isTouching(sanitizer)){
     gameState=END;
   }
 
@@ -67,8 +72,10 @@ function draw() {
   
   
 
-  spawnCoins();  
-  spawnCars();
+  spawnVaccines();  
+  spawnVirus1();
+  spawnVirus2();
+  spawnVirus3();
   drawSprites();
     
   stroke("white");
@@ -81,71 +88,61 @@ function draw() {
     fill("yellow");
     textSize(70);
     text("GAME OVER", 100, 300);
+
+    stroke("yellow");
+    fill("yellow");
+    textSize(35);
+    text("Your Score: "+score, 200, 350);
+
+    stroke("yellow");
+    fill("yellow");
+    textSize(35);
+    text("High Score: "+newScore, 200, 400);
   }
 
   
 }  
 
-function spawnCars(){
+function spawnVirus1(){
+  
+  if (frameCount%175==0){
+   virus1=createSprite(200, -50, 20, 10);
+   virus1.x=100;
+   virus1.velocityY=(7+score);
+   virus1.addImage(carImg);
+   virus1.scale=0.2;
+   virus1.debug=true;
+   virus1.setCollider("circle",0,0,300);
+   virusGroup.add(virus1);
+  }
+}
+function spawnVirus2(){
+  
   if (frameCount%100==0){
-   Car=createSprite(200, -50, 20, 10);
-   Car.scale=1;
-    
-    
-    
-  var rand = Math.round(random(1,6));
-    switch(rand) {
-      case 1:
-        Car.x=300;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-               break;
-      case 2:
-        Car.x=100;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-        
-              break;
-      case 3:
-        Car.x=500;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-        
-              break; 
-      case 4:
-        Car.x=500;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-              break; 
-                      
-      case 5:
-        Car.x=600;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-              break;  
-                      
-      case 6:
-        Car.x=400;
-        Car.velocityY=(7+score);
-        Car.addImage(carImg);
-        Car.scale=0.2;
-              break;         
-      default: break;
-      
-      
-    }
-    
-    
-    CarGroup.add(Car);
+    virus2=createSprite(200, -50, 20, 10);
+    virus2.x=500;
+    virus2.velocityY=(7+score);
+    virus2.addImage(carImg);
+    virus2.scale=0.2;
+    virus2.setCollider("circle",0,0,300);
+    virusGroup.add(virus2);
   }
 }
 
-function spawnCoins(){
+function spawnVirus3(){
+  
+  if (frameCount%125==0){
+    virus3=createSprite(200, -50, 20, 10);
+    virus3.x=300;
+    virus3.velocityY=(7+score);
+    virus3.addImage(carImg);
+    virus3.scale=0.2;
+    virus3.setCollider("circle",0,0,300);
+    virusGroup.add(virus3);
+  }
+}
+
+function spawnVaccines(){
   if (frameCount%70==0){
      
    
@@ -154,22 +151,22 @@ function spawnCoins(){
   var rand3 = Math.round(random(1,3));
     switch(rand3) {
       case 1: 
-        Coin=createSprite(300, -50, 20, 10);
-        Coin.addImage("coin", coinGif); 
-        Coin.scale=0.05;
-        Coin.velocityY=(5+score);
+        vaccine=createSprite(300, -50, 20, 10);
+        vaccine.addImage("coin", coinGif); 
+        vaccine.scale=0.05;
+        vaccine.velocityY=(5+score);
               break;
       case 2: 
-        Coin=createSprite(100, -50, 20, 10);
-        Coin.addImage("coin", coinGif); 
-        Coin.scale=0.05;
-        Coin.velocityY=(5+score);
+        vaccine=createSprite(100, -50, 20, 10);
+        vaccine.addImage("coin", coinGif); 
+        vaccine.scale=0.05;
+        vaccine.velocityY=(5+score);
               break;
       case 3:
-        Coin=createSprite(500, -50, 20, 10);
-        Coin.addImage("coin", coinGif); 
-        Coin.scale=0.05;
-        Coin.velocityY=(5+score);
+        vaccine=createSprite(500, -50, 20, 10);
+        vaccine.addImage("coin", coinGif); 
+        vaccine.scale=0.05;
+        vaccine.velocityY=(5+score);
               break;
       default: break;
     }
@@ -177,7 +174,7 @@ function spawnCoins(){
     
     
     
-  CoinGroup.add(Coin);
+  vaccineGroup.add(vaccine);
   
  }
 }
